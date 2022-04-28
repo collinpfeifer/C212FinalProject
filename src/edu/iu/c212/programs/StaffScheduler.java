5280f68928d140cf21c7b6f7ec0c525332e1c219
Collin Pfeifer <collinpfeifer@Icloud.com>
2022-04-28 11:48:16 -0400

    scheduler staff work

--- src/edu/iu/c212/programs/StaffScheduler.java
+++ src/edu/iu/c212/programs/StaffScheduler.java
@@ -4,6 +4,7 @@ import edu.iu.c212.models.Staff;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 import static edu.iu.c212.utils.FileUtils.readStaffFromFile;
@@ -18,59 +19,59 @@ public class StaffScheduler {
         ArrayList<Staff> staff = (ArrayList<Staff>) readStaffFromFile();
         ArrayList<Double> hours = new ArrayList<>();
         ArrayList<String> schedule = new ArrayList<>();
-        ArrayList<ArrayList<String>> days = new ArrayList<>(7);
-        for(int i = 0; i < 7; i++){
-            days.add(new ArrayList<>());
-        }
+        ArrayList<ArrayList<String>> days = new ArrayList<>(Arrays.asList(new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>()));
         for(Staff s: staff){
             hours.add(0.0);
         }
         // when the person who has worked the least amoutn of hours has fufilled their avaiability
-        loop: while(true) {
+       while(true) {
             ArrayList<Staff> canWork = new ArrayList<>();
-            int min = Integer.MIN_VALUE;
-            ArrayList<String> minDay = new ArrayList<>();
-            for (ArrayList<String> a : days) {
+           ArrayList<String> minDay = new ArrayList<>();
+           int min = Integer.MAX_VALUE;
+           double hoursToWork = 0;
+           double minHours = Integer.MAX_VALUE;
+           int minIndex = 0;
+
+
+           for (ArrayList<String> a : days) {
                 if (a.size() < min) {
                     min = a.size();
                     minDay = a;
                 }
             }
-            double hoursToWork = 0;
             for (Staff s : staff) {
                 String available = s.getAvailability();
                 int index = days.indexOf(minDay);
                 String day = "";
                 switch (index) {
-                    case 0:
+                    case 0 -> {
                         day = "M";
                         hoursToWork = 9;
-                        break;
-                    case 1:
+                    }
+                    case 1 -> {
                         day = "T";
                         hoursToWork = 9;
-                        break;
-                    case 2:
+                    }
+                    case 2 -> {
                         day = "W";
                         hoursToWork = 9;
-                        break;
-                    case 3:
+                    }
+                    case 3 -> {
                         day = "TR";
                         hoursToWork = 9;
-                        break;
-                    case 4:
+                    }
+                    case 4 -> {
                         day = "F";
                         hoursToWork = 9;
-                        break;
-                    case 5:
+                    }
+                    case 5 -> {
                         day = "SAT";
                         hoursToWork = 12.5;
-                        break;
-                    case 6:
+                    }
+                    case 6 -> {
                         day = "SUN";
                         hoursToWork = 12.5;
-                        break;
-
+                    }
                 }
                 if (available.contains(day)) {
                     canWork.add(s);
@@ -78,21 +79,22 @@ public class StaffScheduler {
                     canWork.add(null);
                 }
             }
-            double minHours = Integer.MAX_VALUE;
-            int minIndex = 0;
+
             for (int i = 0; i < canWork.size(); i++) {
-                if (canWork.get(i) == null) {
-                } else if (hours.get(i) < minHours) {
+                if (!(canWork.get(i) == null) && hours.get(i) < minHours) {
                     minHours = hours.get(i);
                     minIndex = i;
                 }
             }
+
             if(days.get(days.indexOf(minDay)).contains(staff.get(minIndex).getName())){
-                break loop;
+                break;
             }
+
             days.get(days.indexOf(minDay)).add(staff.get(minIndex).getName());
-            hours.set(minIndex, hoursToWork);
+            hours.set(minIndex, hoursToWork + hours.get(minIndex));
             System.out.println(hours);
+           System.out.println(days);
         }
     }
 }
